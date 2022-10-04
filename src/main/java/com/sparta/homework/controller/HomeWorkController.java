3fc641d74124d857a4825857d7bbd341a81a2f6a
meruberu <wmj0924@gmail.com>
2022-10-04 11:14:38 +0900

    URI 수정

--- src/main/java/com/sparta/homework/controller/HomeWorkController.java
+++ src/main/java/com/sparta/homework/controller/HomeWorkController.java
@@ -16,31 +16,31 @@ public class HomeWorkController {
     private final HomeWorkRepository homeWorkRepository;
     private  final HomeWorkService homeWorkService;
 
-    @GetMapping("/api/homework")
+    @GetMapping("/api/post")
     public List<HomeWork> getHomeWork() {
         return homeWorkService.getpost();
     }
 
-    @PostMapping("/api/homework")
+    @PostMapping("/api/post")
     public Long homeWorkmemo(@RequestBody HomeWorkRequestDto requestDto) {
         return homeWorkService.post(requestDto);
     }
-    @DeleteMapping("/api/homework/{id}")
+    @DeleteMapping("/api/post/{id}")
     public Long deleteMemo(@PathVariable Long id) {
         homeWorkService.delete(id);
         return id;
     }
-    @PutMapping("/api/homework/{id}")
+    @PutMapping("/api/post/{id}")
     public Long updateMemo(@PathVariable Long id, @RequestBody HomeWorkRequestDto requestDto) {
         homeWorkService.update(id, requestDto);
         return id;
     }
 
-    @GetMapping("/api/homework/{id}")
+    @GetMapping("/api/post/{id}")
     public HomeWork getone(@PathVariable Long id) {
         return homeWorkService.getone(id);
     }
-    @PostMapping("/api/homework/{id}")
+    @PostMapping("/api/post/{id}")
     public boolean check(@PathVariable Long id, @RequestBody HomeWorkRequestDto requestDto) {
         return homeWorkService.check(id, requestDto);
     }
